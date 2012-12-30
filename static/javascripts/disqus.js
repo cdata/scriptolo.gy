3813d40e6afe576f1493256caf3642c9cc833b9e
Chris Joel <chris@scriptolo.gy>
2012-12-29 17:52:51 -0800

    Disqus needs to load with a matching protocol..

--- static/javascripts/disqus.js
+++ static/javascripts/disqus.js
@@ -3,7 +3,7 @@ define(['jquery', 'underscore'],
 
   function loadDisqusScript() {
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://cdata-scriptology.disqus.com/embed.js';
+    dsq.src = '//cdata-scriptology.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] ||
       document.getElementsByTagName('body')[0]).appendChild(dsq);
   }
